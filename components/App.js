8c39f24041cb4e55bf6adc96a3ba937b2c33e1c0
Natalia Jędrosz <n.jedrosz@gmail.com>
2018-11-11 15:27:36 +0100

    Done

--- components/App.js
+++ components/App.js
@@ -10,7 +10,7 @@ App = React.createClass({
     };
   },
 
-  getGif: function (searchingText, callback) {  // Na wejście metody getGif przyjmujemy dwa parametry: wpisywany tekst (searchingText) i funkcję, która ma się wykonać po pobraniu gifa (callback)
+  getGif: function (searchingText) {  // Na wejście metody getGif przyjmujemy dwa parametry: wpisywany tekst (searchingText)
     
     return new Promise(
       function(resolve, reject) {
@@ -24,7 +24,7 @@ App = React.createClass({
               url: data.fixed_width_downsampled_url,
               sourceUrl: data.url
             };
-            resolve(callback(gif));
+            resolve(gif); //zwracamy gifa
           } else {
             reject(new Error(
               `XMLHttpRequest Error: coś poszło nie tak`));
@@ -35,34 +35,19 @@ App = React.createClass({
 
   },
 
-  // function (searchingText, callback) {  // Na wejście metody getGif przyjmujemy dwa parametry: wpisywany tekst (searchingText) i funkcję, która ma się wykonać po pobraniu gifa (callback)
-  //   var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // Konstruujemy adres URL dla API Giphy
-  //   var xhr = new XMLHttpRequest();  // Wywołujemy całą sekwencję tworzenia zapytania XHR do serwera i wysyłamy je
-  //   xhr.open('GET', url);
-  //   xhr.onload = function () {
-  //     if (xhr.status === 200) {
-  //       var data = JSON.parse(xhr.responseText).data; // W obiekcie odpowiedzi mamy obiekt z danymi. W tym miejscu rozpakowujemy je sobie do zmiennej data, aby nie pisać za każdym razem response.data
-  //       var gif = {  // Układamy obiekt gif na podstawie tego, co otrzymaliśmy z serwera
-  //         url: data.fixed_width_downsampled_url,
-  //         sourceUrl: data.url
-  //       };
-  //       callback(gif);  // Przekazujemy obiekt do funkcji callback, którą przekazaliśmy jako drugi parametr metody getGif
-  //     }
-  //   };
-  //   xhr.send();
-  // },
-
   handleSearch: function (searchingText) {  // Pobierz na wejściu wpisywany tekst
+    
     this.setState({
       loading: true  // Zasygnalizuj, że zaczął się proces ładowania
     });
-    this.getGif(searchingText, function (gif) {  // Rozpocznij pobieranie gifa
-      this.setState({  // Na zakończenie pobierania:
+    this.getGif(searchingText)
+      .then(gif => this.setState({  // Na zakończenie pobierania:
         loading: false,  // przestań sygnalizować ładowanie
         gif: gif,  // ustaw nowego gifa z wyniku pobierania
         searchingText: searchingText  // ustaw nowy stan dla wyszukiwanego tekstu
-      });
-    }.bind(this));
+      }))
+      .catch(error => console.log(error));
+  
   },
 
   render: function () {
